6f928ee67fe9bfdf448df9a302a513f53a5e69ce
Kirill Yashin <34216147+KirillYashin@users.noreply.github.com>
2021-04-16 20:25:45 +0300

    Create Exception.h

---
+++ List/Exception/Exception.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Exception
+{
+private:
+	std::string str;
+public:
+	Exception(std::string _str) : str(_str) {}
+	void Print() { std::cout << "Here comes the Problem!" << std::endl << str << std::endl; }
+};
